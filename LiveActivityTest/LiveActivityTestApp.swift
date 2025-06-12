b06b94f2ad2d79e6de83b037bad3a9d7b2d4afb5
Carl Xu <carl92xu@gmail.com>
2025-06-12 01:19:28 -0500

    Initial Commit

---
+++ LiveActivityTest/LiveActivityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityTestApp.swift
+//  LiveActivityTest
+//
+//  Created by carl on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveActivityTestTests/LiveActivityTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityTestTests.swift
+//  LiveActivityTestTests
+//
+//  Created by carl on 6/12/25.
+//
+
+import Testing
+@testable import LiveActivityTest
+
+struct LiveActivityTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
